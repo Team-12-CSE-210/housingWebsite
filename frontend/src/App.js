e2c9d5daab522dc565c3117e6b720d159020e9d0
Shril Mody <shmody@ucsd.edu>
2023-03-09 21:36:48 -0800

    fifth change -rebase

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import SelectDropdown from './components/Select_Dropdown';
 import RatingSelectVariants from './components/Rating';
 import MultipleCheckbox from './components/Multiple_Checkbox';
+import Header from './components/Header'
+import Footer from './components/Footer';
+import PriceRange from './components/PriceRange';
 
 const amenities = ['Gym','Pool','Laundry','Pets','Ac/Heating','Parking','Wifi','TV',];
 const housetypes = ['House','Apartment','Condo','Townhouse','Studio','Loft','Other'];
@@ -14,6 +17,7 @@ function App() {
   return (
     <div className="App">
       <SearchBar placeholder="Enter Address/Name/Zipcode" data={Data}/>
+      <PriceRange />
       <MultipleCheckbox Name="House Type" types={housetypes}> </MultipleCheckbox>
       <RatingSelectVariants />
       <SelectDropdown Name="Bed" types={number} InputLabel="# of Bed"></SelectDropdown>
