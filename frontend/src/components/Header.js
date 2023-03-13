50e13458b445f528e110229e5576bf7b8338f68f
Brian_XiaoKe <41765046+xilohuang@users.noreply.github.com>
2023-03-13 13:28:48 -0700

    header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,32 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/Header.css'
+import '../styles/Header.css';
+import logo from '../logo.svg';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Header(props) {
     const { currUser } = props;
     return (
-        <div className='header'>
-            <div className='navigation-links'>
-                <Link to="/"> Lysta </Link>
-                <Link to="/rent"> Rent </Link>
-                <Link to="/list"> List </Link>
-                <Link to="/help"> Help </Link>
-            </div>
-            <div className='account-buttons'>
-                {currUser ? (
-                    <Link to="/login">
-                        <button>Hi {currUser}!</button>
+        <Navbar bg="dark" variant="dark" className='navpad'>
+            <Container className='maxwidth'>
+                <Navbar.Brand href="/">
+                    Lysta
+                </Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="/rent" className='text1'>Rent</Nav.Link>
+                    <Nav.Link href="/list" className='text1'>List</Nav.Link>
+                    <Nav.Link href="/help" className='text1'>Help</Nav.Link>
+                </Nav>
+
+                <div className='account-buttons'>
+                    {currUser ? (
+                        <Link to="/" className='account-buttons' role='button'>
+                            Hi {currUser}!
+                        </Link>
+                    ) : (
+                        <Link to="/login" className='account-buttons' role='button'>
+                            Login
+                        </Link>
+                    )}
+                    <Link to="/signup" className='account-buttons' role='button'>
+                        Sign Up
                     </Link>
-                ) : (
-                    <Link to="/login">
-                        <button>Login</button>
-                    </Link>
-                )}
-                <Link to="/signup">
-                    <button>Sign Up</button>
-                </Link>
-            </div>
-        </div>
+                </div>
+            </Container>
+        </Navbar>
     );
 }
 
